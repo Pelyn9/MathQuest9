0bc6fd508cbf219d9e9ec53f391a51d6747bc2a2
Pelyn9 <peejaymarco@gmail.com>
2026-05-12 01:43:41 +0800

    feat: Update asset paths and dependencies for consistency and compatibility

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 import 'react-native-gesture-handler';
-import { AppRegistry, Platform } from 'react-native';
+import { registerRootComponent } from 'expo';
 import App from './App';
 
-AppRegistry.registerComponent('main', () => App);
-
-if (Platform.OS === 'web' && typeof window !== 'undefined') {
-  const rootTag = document.getElementById('root') || document.getElementById('main');
-  if (rootTag) {
-    AppRegistry.runApplication('main', { rootTag });
-  }
-}
+registerRootComponent(App);
